ddb7c5e7b13ac1dd046f2b472b22be39715399ee
Juan David Lis <juan-dy-s@hotmail.com>
2021-08-21 20:44:38 -0500

    database

--- database/db.config.js
+++ database/db.config.js
@@ -1,8 +1,8 @@
 const dbconfig = {
-    HOST: "localhost",
-    USER: "userpoli",
-    PASSWORD: "Poli-1234",
-    DB: "dbPoliEventos",
+    HOST: "us-cdbr-east-04.cleardb.com",
+    USER: "be40ef70e2d081",
+    PASSWORD: "ccd5cf91",
+    DB: "heroku_4bae8b19ecc8795",
     dialect: "mysql",
     pool:{
         max: 5,
